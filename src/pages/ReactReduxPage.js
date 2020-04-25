a7e79f95ba71888a807e1e4ec5aa8e7310053d54
郭建利 <guojianli@ihappy.net.cn>
2020-04-25 14:05:54 +0800

    实现bindActionCreators函数

--- src/pages/ReactReduxPage.js
+++ src/pages/ReactReduxPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+// import { bindActionCreators } from 'redux'
+import { bindActionCreators } from '../Jreact-redux/'
 
 export default connect(
   ({ count }) => ({ count }),
